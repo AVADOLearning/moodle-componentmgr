d3fc1e42b48e62d3a9235b1aea475cf701a316d7
Luke Carrier <luke@carrier.im>
2015-06-28 10:21:23 +0100

    Let's set up error reporting, too

--- etc/bootstrap.php
+++ etc/bootstrap.php
@@ -8,6 +8,11 @@
  * @license GPL v3
  */
 
+// Error reporting
+ini_set('display_errors',  'on');
+ini_set('error_reporting', E_ALL);
+
+// Set the default timezone if not already set
 if (!ini_get('date.timezone')) {
     date_default_timezone_set(@date_default_timezone_get());
 }
