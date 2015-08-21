23332f93665db6d165b2366d38251886dfebd7ef
Luke Carrier <luke@carrier.im>
2015-08-21 11:16:02 +0100

    Flesh out Moodle plugin

---
+++ src-local_componentmgr/lang/en/local_componentmgr.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Moodle component manager.
+ *
+ * @author Luke Carrier <luke@carrier.im>
+ * @copyright 2015 Luke Carrier
+ * @license GPL v3
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+// Moodle component metadata
+$string['pluginname'] = 'Component Manager support library';
