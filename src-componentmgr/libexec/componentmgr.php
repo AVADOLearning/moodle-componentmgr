22a57714ca52405244fd173c57cf58a0170686d4
Luke Carrier <Luke.Carrier@floream.com>
2015-10-01 11:59:32 +0100

    Drop unused dependency

--- src-componentmgr/libexec/componentmgr.php
+++ src-componentmgr/libexec/componentmgr.php
@@ -9,7 +9,6 @@
  * @license GPL v3
  */
 
-use ComponentManager\ComponentManagerApplication;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
