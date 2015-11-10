177a211ede55bc1c05456826d9473c781cec4604
Luke Carrier <Luke.Carrier@floream.com>
2015-11-10 14:23:42 +0000

    Windows makes me sad
    Fix command line argument parsing on Windows, where --long-options were
    ignored.

--- src/bin/componentmgr.bat
+++ src/bin/componentmgr.bat
@@ -18,7 +18,7 @@ php -d variables_order=EGPCS ^
     -d error_reporting=-1 ^
     -d display_errors=On ^
     -d curl.cainfo=!caBundle! ^
-    !rootDir!\libexec\componentmgr.php -- %*
+    !rootDir!\libexec\componentmgr.php %*
 SET errno=%errorlevel%
 EXIT /B !errno!
 
