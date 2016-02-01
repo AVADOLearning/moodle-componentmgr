92101ba87c63424e70045d19cc43fbc67ef03be7
Luke Carrier <luke@carrier.im>
2016-02-01 21:57:38 +0000

    Fix MoodlePackageRepository::satisfiesVersion() type juggling

--- lib/PackageRepository/MoodlePackageRepository.php
+++ lib/PackageRepository/MoodlePackageRepository.php
@@ -197,7 +197,7 @@ class MoodlePackageRepository extends AbstractPackageRepository
      * @override \ComponentManager\PackageRepository\PackageRepository
      */
     public function satisfiesVersion($versionSpecification, ComponentVersion $version) {
-        return (string) $versionSpecification === $version->getVersion()
-                || (string) $versionSpecification === $version->getRelease();
+        return $versionSpecification == $version->getVersion()
+                || $versionSpecification == $version->getRelease();
     }
 }
