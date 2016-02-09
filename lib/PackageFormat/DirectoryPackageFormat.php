491170c598655ef513a169f19e953d2e5cd67a0e
Luke Carrier <luke@carrier.im>
2016-02-09 14:34:46 +0000

    Directory package format for "pass through" style deploy

---
+++ lib/PackageFormat/DirectoryPackageFormat.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Moodle component manager.
+ *
+ * @author Luke Carrier <luke@carrier.im>
+ * @copyright 2016 Luke Carrier
+ * @license GPL-3.0+
+ */
+
+namespace ComponentManager\PackageFormat;
+
+use ComponentManager\Project\ProjectFile;
+use ComponentManager\Project\ProjectLockFile;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Directory package format.
+ *
+ * Allows copying a packaged instance directly to the target location.
+ */
+class DirectoryPackageFormat extends AbstractPackageFormat
+        implements PackageFormat {
+    /**
+     * @override \ComponentManager\PackageFormat\PackageFormat
+     */
+    public function package($moodleDir, $destination,
+                            ProjectFile $projectFile,
+                            ProjectLockFile $projectLockFile,
+                            LoggerInterface $logger) {
+        $this->filesystem->mirror($moodleDir, $destination);
+    }
+}
