3cc89b7a7ccd28033256a8c8d81852c1267ebaf7
Luke Carrier <luke@carrier.im>
2017-09-16 12:37:58 +0100

    tests: fix broken integration tests

--- test/integration/default/serverspec/install_command_spec.rb
+++ test/integration/default/serverspec/install_command_spec.rb
@@ -4,11 +4,11 @@ describe 'install command' do
   describe file('/tmp/install/moodle/componentmgr.lock.json') do
     it { should be_a_file }
 
-    its(:content) { should match /"componentName": *"cachestore_redis"/ }
-    its(:content) { should match /"finalVersion": *"[a-f0-9]{40}"/ }
+    its(:content) { should contain /"componentName": *"cachestore_redis"/ }
+    its(:content) { should contain /"finalVersion": *"[a-f0-9]{40}"/ }
 
-    its(:content) { should match /"componentName": *"local_cpd"/ }
-    its(:content) { should match /"md5Checksum": *"[a-f0-9]{32}"/ }
+    its(:content) { should contain /"componentName": *"local_cpd"/ }
+    its(:content) { should contain /"md5Checksum": *"[a-f0-9]{32}"/ }
   end
 
   describe file('/tmp/install/moodle/cache/stores/redis/version.php') do
@@ -17,6 +17,6 @@ describe 'install command' do
 
   describe file('/tmp/install/moodle/local/cpd/version.php') do
     it { should be_a_file }
-    its(:content) { should match /\$plugin->release *= *('|")0\.4\.0('|");/ }
+    its(:content) { should contain /\$plugin->release *= *('|")0\.4\.0('|");/ }
   end
 end
--- test/integration/default/serverspec/package_command_spec.rb
+++ test/integration/default/serverspec/package_command_spec.rb
@@ -4,19 +4,19 @@ describe 'package command' do
   describe file('/tmp/package/componentmgr.lock.json') do
     it { should be_a_file }
 
-    its(:content) { should match /"componentName": *"cachestore_redis"/ }
-    its(:content) { should match /"finalVersion": *"[a-f0-9]{40}"/ }
+    its(:content) { should contain /"componentName": *"cachestore_redis"/ }
+    its(:content) { should contain /"finalVersion": *"[a-f0-9]{40}"/ }
 
-    its(:content) { should match /"componentName": *"local_cpd"/ }
-    its(:content) { should match /"md5Checksum": *"[a-f0-9]{32}"/ }
+    its(:content) { should contain /"componentName": *"local_cpd"/ }
+    its(:content) { should contain /"md5Checksum": *"[a-f0-9]{32}"/ }
 
-    its(:content) { should match /"componentName": *"local_componentmgrtest"/ }
-    its(:content) { should match /"finalVersion": *null/ }
+    its(:content) { should contain /"componentName": *"local_componentmgrtest"/ }
+    its(:content) { should contain /"finalVersion": *null/ }
   end
 
   describe file('/tmp/package/version.php') do
     it { should be_a_file }
-    its(:content) { should match /\$release *= *('|")3\.1\.[0-9]+\+ \(Build: [0-9]+\)('|");/ }
+    its(:content) { should contain /\$release *= *('|")3\.1\.[0-9]+\+? *\(Build: [0-9]+\)('|");/ }
   end
 
   describe file('/tmp/package/cache/stores/redis/version.php') do
@@ -25,7 +25,7 @@ describe 'package command' do
 
   describe file('/tmp/package/local/cpd/version.php') do
     it { should be_a_file }
-    its(:content) { should match /\$plugin->release *= *('|")0\.4\.0('|");/ }
+    its(:content) { should contain /\$plugin->release *= *('|")0\.4\.0('|");/ }
   end
 
   describe file('/tmp/package/local/componentmgrtest/version.php') do
