753780d2de11ec8c284c6b85dea9253367ab58cb
Luke Carrier <luke@carrier.im>
2017-10-10 22:36:22 +0100

    travis: use Docker service over manual install

--- test/travis/after_fail.sh
+++ test/travis/after_fail.sh
@@ -6,12 +6,15 @@ strict_enable
 
 travis_fold start logs.kitchen
     travis_section '$ cat .kitchen/logs/kitchen.log'
-
     cat '.kitchen/logs/kitchen.log'
 travis_fold end logs.kitchen
 
 travis_fold start versions.docker
     travis_section '$ docker version'
-
     docker version
 travis_fold end versions.docker
+
+travis_fold start diagnose.kitchen
+    travis_section '$ bundle exec kitchen diagnose'
+    bundle exec kitchen diagnose
+travis_fold end diagnose.kitchen
